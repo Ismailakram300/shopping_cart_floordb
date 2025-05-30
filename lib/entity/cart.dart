aa4042d360e9dca47d17740a49843dba0b68592b
Ismailakram300 <ismailakram300@gmail.com>
2025-05-31 02:09:04 +0500

    commit

--- lib/entity/cart.dart
+++ lib/entity/cart.dart
@@ -7,6 +7,15 @@ class cart {
   @primaryKey
   final int? id;
 
-  
+  String? name;
+  String? uid;
+  String? category;
+  String? imageUrl;
+  String? oldPrice;
+  String? price;
+
+  cart({this.uid, this.id, this.name, this.category, this.imageUrl, this.oldPrice,
+    this.price});
+
 
 }
