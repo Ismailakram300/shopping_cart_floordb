cf1fe37376acf350c61e2df33b1ed13041a27f28
Ismailakram300 <ismailakram300@gmail.com>
2025-05-31 02:15:15 +0500

    commit

--- lib/dao/dao.dart
+++ lib/dao/dao.dart
@@ -13,6 +13,9 @@ abstract class CartDao{
 @Query('SELECT * FROM CART WHERE uid:uid AND id:id')
   Stream<List<cart>> getItemCartByUid(String uid,id);
 
+@Query('DELETE FROM CART WHERE uid:uid')
+  Stream<List<cart>> deleteItemCartByUid(String uid);
+
 
   
 }
