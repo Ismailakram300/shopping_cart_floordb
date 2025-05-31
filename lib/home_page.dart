9610051abd45b1584ebc6fb49b651336c240053c
Ismailakram300 <ismailakram300@gmail.com>
2025-06-01 02:59:01 +0500

    commi

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,19 +5,6 @@ class MyHomePage extends StatefulWidget {
   final CartDao dao;
   const MyHomePage({required this.dao});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(appBar: AppBar(title: Text("HomePage"))
-      
       ///body: FutureBuilder(future: readJSONDatabase(), builder: builder),
       
       
